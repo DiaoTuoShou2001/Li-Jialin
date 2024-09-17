ca080f0b5a31484cd3a6b69e2a0e09b55e2f2358
DiaoTuoShou <lijialin2077@163.com>
2024-09-17 15:27:34 +0800

    richanggengxin

--- mydz1.cpp
+++ mydz1.cpp
@@ -1,10 +1,48 @@
 #include "any"
 #include "ostream"
+#include "string"
 
 class guandao {
 
 private:
-} int main(int argc, char *argv[]) {
+  std::string name; // 管道名称
+  double length;    // 管道长度
+  bool underMaintenance;
+  // guandao 管道
+public:
+  guandao(const std::string &name, double length, bool underMaintenance)
+      : name(name), length(length), underMaintenance(underMaintenance) {}
 
-  return 0;
+  guandao() : length(0.0), underMaintenance(false) {}
+
+  void setname(const std::string newname) { name = newname; }
+  void std::string getname() const { return name; }
+
+  void setlenth(double newlenth) {
+    if (newlenth > 0) {
+      length = newlenth;
+    } else {
+      std::cerr << "cuowuchangdu" << std::endl;
+    }
+  }
+
+  void getlenth() const { return length; }
+  void setmaint(bool isunder) { underMaintenance = isunder; }
+  void getmaint() const { return underMaintenance; }
+
+  void displayifo() {
+    std::cout << "Pipe name:" << name << "\nlength:" << length
+              << "\nUnder Maintenance:" << (underMaintenance ? "yes" : "no")
+              << std::endl;
+  }
+} 
+
+
+
+
+int main() {
+  guandao1 = guandao("shenzhou", 250.0, flase);
+  guandao1.displayifo()
+
+      return 0;
 }
