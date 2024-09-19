7aa3ab603cc61f7c8bd9ea2524e0a54391aa809d
DiaoTuoShou <lijialin2077@163.com>
2024-09-19 17:38:14 +0800

    richang

--- mydz1.cpp
+++ mydz1.cpp
@@ -1,6 +1,7 @@
 #include "any"
 #include "ostream"
-#include "string"
+#include <cmath>
+#include <string>
 
 class guandao {
 
@@ -10,10 +11,10 @@ private:
   bool underMaintenance;
   // guandao 管道
 public:
-  guandao(const std::string &name, double length, bool underMaintenance)
+  guandao(const std::string name, double length, bool underMaintenance)
       : name(name), length(length), underMaintenance(underMaintenance) {}
 
-  guandao() : length(0.0), underMaintenance(false) {}
+  guandao() : name(""), length(0.0), underMaintenance(false) {}
 
   void setname(const std::string newname) { name = newname; }
   void std::string getname() const { return name; }
@@ -35,14 +36,45 @@ public:
               << "\nUnder Maintenance:" << (underMaintenance ? "yes" : "no")
               << std::endl;
   }
-} 
+} class bengzhan {
+private:
+  std::string name;
+  int number;
+  int worknumber;
+  double xiaolv;
 
+public:
+  bengzhan(const std::string name,shuliang(shuliang),yunxingshuliang(yunxingshuliang),xiaolv(xiaolv)) : name(name), numberinumber), worknumber(worknumber),xiaolv(xiaolv) {}
 
+  bengzhan() : name(""), number(0), worknumber(0), xiaolv(0.0) {}
+
+  void setname(const std::string newname) { name = newname; }
+  void std::string getname() const { return name; }
+
+  void setnumber(int newnumber) {
+    if (newnumber > 0) {
+      number = newnumber;
+    } else {
+      std::cerr << "shuliangwubunengwei 0 " << std::endl;
+    }
+  }
+  void setworknumber(int newworknumber) { worknumber = newworknumber; }
+  void setxiaolv(double newxiaolv) { xiaolv = newxiaolv; }
+  void getnumber() const { return number; }
+  void getworknumber() const { return worknumber; }
+  void getxiaolv() const { return xiaolv; }
+  void displayifo() {
+    std::cout << "Pipe name:" << name << "\nlength:" << length
+              << "\nUnder Maintenance:" << (underMaintenance ? "yes" : "no")
+              << std::endl;
+  }
+
+}
 
 
 int main() {
   guandao1 = guandao("shenzhou", 250.0, flase);
-  guandao1.displayifo()
+  guandao1.displayifo();
 
-      return 0;
+  return 0;
 }
